28d172c2142133e342bd03ad9c0188aef3e8f57f
ilyab <i-baykov@internet.ru>
2023-02-17 18:46:49 +0300

    Динамический URL

--- days/url.py
+++ days/url.py
@@ -2,13 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    path('monday', views.monday),
-    path('tuesday', views.tuesday),
-    path('wednesday', views.wednesday),
-    path('thursday', views.thursday),
-    path('friday', views.friday),
-    path('saturday', views.saturday),
-    path('sunday', views.sunday),
+    path('<param>', views.param)
 
 ]
--- days/views.py
+++ days/views.py
@@ -4,30 +4,21 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def monday(request):
-    return HttpResponse("Понедельник день тяжелый ;с")
 
-
-def tuesday(request):
-    return HttpResponse("Вторник день тяжелый ;с")
-
-
-def wednesday(request):
-    return HttpResponse("Среда день тяжелый ;с")
-
-
-def thursday(request):
-    return HttpResponse("Четверг день тяжелый ;с")
-
-
-def friday(request):
-    return HttpResponse("Пятница день тяжелый ;с")
-
-
-def saturday(request):
-    return HttpResponse("Суббота день тяжелый ;с")
-
-
-def sunday(request):
-    return HttpResponse("Воскресенье  день тяжелый ;с")
-
+def param(request, param):
+    if param == "monday":
+        return HttpResponse("Понедельник день тяжелый ;с")
+    elif param == "tuesday":
+        return HttpResponse("Вторник день тяжелый ;с")
+    elif param == "wednesday":
+        return HttpResponse("Среда день тяжелый ;с")
+    elif param == "thursday":
+        return HttpResponse("Четверг день тяжелый ;с")
+    elif param == "friday":
+        return HttpResponse("Пятница день тяжелый ;с")
+    elif param == "saturday":
+        return HttpResponse("Суббота день тяжелый ;с")
+    elif param == "sunday":
+        return HttpResponse("Воскресенье  день тяжелый ;с")
+    else:
+        return HttpResponse("Я не знаю так день недели ((((", param)
